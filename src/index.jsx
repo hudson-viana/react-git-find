8bafe80acd6b1715fbdf99e9f17b2a1a8ef168eb
Hudson Santos <work.of.hudson@gmail.com>
2024-02-08 12:46:40 -0300

    feat: add header jsx and css && add general css && add google fonts links to html

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './src/Home'
+import App from './pages/Home'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
