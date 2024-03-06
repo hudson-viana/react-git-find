039ed98c9a83b09444e5a2fc6616aba80daa090f
Hudson Santos <work.of.hudson@gmail.com>
2024-03-06 12:55:32 -0300

    feat: update readme, add background image, add ItemList component for the repos list (js and css), add Dynamic Rendering, Asynchronous Data Fetching and Conditional Rendering

--- README.md
+++ README.md
@@ -1,8 +1,30 @@
+# Repo Description
+This repository contains a React application for searching GitHub users and displaying their information and repositories.
+
+# Features
+
+
+- User Search: Utilizes the GitHub API to search for users based on their username.
+- User Information Display: Retrieves and displays user details such as name, username, bio, and profile picture.
+- Repository Listing: Fetches and presents a list of repositories belonging to the searched user.
+- Dynamic Rendering: Utilizes React's state management to dynamically render user information and repositories.
+
+# Technologies Used
+
+- React: Built using React library for front-end development.
+- useState Hook: Implements state management for user input and data retrieval.
+- GitHub API: Fetches user data and repositories from the GitHub API.
+- CSS Styles: Applies custom CSS styles for layout and presentation.
+
+# Solutions Introduced
+
+- Asynchronous Data Fetching: Implements asynchronous data fetching using fetch and `await` for seamless user experience.
+- Conditional Rendering: Employs conditional rendering to display user information and repositories based on availability.
+
 # React + Vite
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
-Currently, two official plugins are available:
+# Starting dev server
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+$ npm run dev
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,80 @@
-import { Header } from '../../components/Header';
+import { useState } from "react";
+import { Header } from "../../components/Header";
+import ItemList from "../../components/ItemList";
+import "./styles.css";
 
 function App() {
-    return (
-        <div className="App">
-            <Header />
+  const [user, setUser] = useState('');
+  const [currentUser, setCurrentUser] = useState(null);
+  const [repos, setRepos] = useState(null);
+
+  const handleGetData = async () => {
+    const userData = await fetch(`https://api.github.com/users/${user}`);
+    const newUser = await userData.json();
+    
+    if(newUser.name) {
+      const {avatar_url, name, bio, login} = newUser;
+      setCurrentUser({avatar_url, name, bio, login});
+
+      const reposData = await fetch(`https://api.github.com/users/${user}/repos`);
+      const newRepos = await reposData.json();
+
+      if(newRepos.length) {
+        setRepos(newRepos);
+      }
+    }
+  }
+  
+  
+  
+  return (
+    <div className="App">
+      <Header />
+      <div className="content">
+        <img src="src/assets/background.png" className="background" alt="background image" />
+        <div className="info">
+          <div className="search-section">
+            <input 
+            type="text" 
+            name="username" 
+            placeholder="@username"
+            value={user}
+            onChange={event => setUser(event.target.value)} />
+            <button onClick={handleGetData}>Search</button>
+          </div>
+            {currentUser?.name ? (
+              <>
+              <div className="profile-container">
+                <div className="profile-image">
+                  <img
+                    src={currentUser.avatar_url}
+                    alt="profile image"
+                  />
+                </div>
+                <div className="profile-text">
+                  <h3>{currentUser.name}</h3>
+                  <span>@{currentUser.login}</span>
+                  <p>{currentUser.bio}</p>
+                </div>
+              </div>
+            <hr />
+            </>
+            ) : null}
+        {repos?.length ? (
+
+          <div className="repositories">
+            <h4>Repositories</h4>
+            {repos.map(repo => (
+              <ItemList title={repo.name} description={repo.description} />
+
+            ))}
+          </div>
+
+        ) : null}
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
