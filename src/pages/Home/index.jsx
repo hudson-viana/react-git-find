dd8c15e3eb7801b01f1ddb7776534672c95e6db6
Hudson Santos <work.of.hudson@gmail.com>
2024-07-15 12:47:12 -0300

    feat(input): 'enter' to seach and fix(repos.map): add key={index}

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,52 +4,61 @@ import ItemList from "../../components/ItemList";
 import "./styles.css";
 
 function App() {
-  const [user, setUser] = useState('');
+  const [user, setUser] = useState("");
   const [currentUser, setCurrentUser] = useState(null);
   const [repos, setRepos] = useState(null);
 
   const handleGetData = async () => {
     const userData = await fetch(`https://api.github.com/users/${user}`);
     const newUser = await userData.json();
-    
-    if(newUser.name) {
-      const {avatar_url, name, bio, login} = newUser;
-      setCurrentUser({avatar_url, name, bio, login});
 
-      const reposData = await fetch(`https://api.github.com/users/${user}/repos`);
+    if (newUser.name) {
+      const { avatar_url, name, bio, login } = newUser;
+      setCurrentUser({ avatar_url, name, bio, login });
+
+      const reposData = await fetch(
+        `https://api.github.com/users/${user}/repos`
+      );
       const newRepos = await reposData.json();
 
-      if(newRepos.length) {
+      if (newRepos.length) {
         setRepos(newRepos);
       }
     }
-  }
-  
-  
-  
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      handleGetData();
+    }
+  };
+
   return (
     <div className="App">
       <Header />
       <div className="content">
-        <img src="src/assets/background.png" className="background" alt="background image" />
+        <img
+          src="src/assets/background.png"
+          className="background"
+          alt="background image"
+        />
         <div className="info">
           <div className="search-section">
-            <input 
-            type="text" 
-            name="username" 
-            placeholder="@username"
-            value={user}
-            onChange={event => setUser(event.target.value)} />
+            <input
+              type="text"
+              name="username"
+              placeholder="@username"
+              value={user}
+              onKeyDown={handleKeyDown}
+              onChange={(event) => setUser(event.target.value)}
+            />
             <button onClick={handleGetData}>Search</button>
           </div>
-            {currentUser?.name ? (
-              <>
+          {currentUser?.name ? (
+            <>
               <div className="profile-container">
                 <div className="profile-image">
-                  <img
-                    src={currentUser.avatar_url}
-                    alt="profile image"
-                  />
+                  <img src={currentUser.avatar_url} alt="profile image" />
                 </div>
                 <div className="profile-text">
                   <h3>{currentUser.name}</h3>
@@ -57,20 +66,21 @@ function App() {
                   <p>{currentUser.bio}</p>
                 </div>
               </div>
-            <hr />
+              <hr />
             </>
-            ) : null}
-        {repos?.length ? (
-
-          <div className="repositories">
-            <h4>Repositories</h4>
-            {repos.map(repo => (
-              <ItemList title={repo.name} description={repo.description} />
-
-            ))}
-          </div>
-
-        ) : null}
+          ) : null}
+          {repos?.length ? (
+            <div className="repositories">
+              <h4>Repositories</h4>
+              {repos.map((index, repo) => (
+                <ItemList
+                  key={index}
+                  title={repo.name}
+                  description={repo.description}
+                />
+              ))}
+            </div>
+          ) : null}
         </div>
       </div>
     </div>
